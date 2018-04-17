65c26099233afd98f5fee97aa0a18e03ae3b93e9
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2018-04-17 10:57:30 +0200

    Gate component draft

--- src/cerberus/components/Gate.jsx
+++ src/cerberus/components/Gate.jsx
@@ -1,19 +1,53 @@
 // @flow
 import * as React from 'react';
+import Predicate from 'predicate';
 import invariant from 'invariant';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import type { AuthConfig, Roles, WrapperFunction, PermissionPredicate, Permissions } from './types';
+import type { AuthConfig, Permissions } from '../types';
 import closurize from '../utils/closurize';
 
 type GateProps = {
     onlyLogin?: boolean,
     role?: string,
     children: React.Node,
-    permissions?: Array<string>,
+    selectedPermissions: Array<string>,
+    permissions: Array<Permissions>,
+    userObject: any,
+    userRole: string,
+    authConfig: AuthConfig,
+    availableRoles: Array<string>,
+    action?: (authState: string) => {},
 };
 
-class Gate extends React.Component <GateProps> {
+const AUTH_SUCCESSFUL = 'authSuccess';
+const AUTH_FAILED = 'authFailed';
+
+const defaultPredicate = [() => true];
+
+const mapStateToProps = ({ authProvider, authProvider: { internals: { permissions } }, ...state }) => ({
+  userObject: authProvider.userObject,
+  userRole: authProvider.userRole,
+  authConfig: authProvider.internals,
+  availableRoles: authProvider.internals.roles,
+  permissions: permissions && permissions.map(p => ({ name: p.name, predicates: p.predicates.map(f => closurize(state, f)) })),
+});
+
+const mapDispatchToProps = (dispatch, { authConfig: { reduxAction } }) => ({
+  action: reduxAction ? result => dispatch(reduxAction(result)) : undefined,
+});
+
+class Gate extends React.Component <GateProps, { permissions: Array<any> }> {
     static propTypes = {
+      permissions: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string,
+        predicates: PropTypes.arrayOf(PropTypes.func),
+      })),
+      userRole: PropTypes.string,
+      /* eslint-disable react/forbid-prop-types */
+      userObject: PropTypes.any,
+      availableRoles: PropTypes.arrayOf(PropTypes.string),
       onlyLogin: PropTypes.bool,
       role: PropTypes.string,
       children: PropTypes.oneOfType([
@@ -22,8 +56,17 @@ class Gate extends React.Component <GateProps> {
       ]).isRequired,
     }
 
-    state = {
+    static defaultProps = {
       permissions: [],
+      userRole: undefined,
+      userObject: undefined,
+      availableRoles: [],
+      onlyLogin: undefined,
+      role: undefined,
+    };
+
+    state = {
+      permissions: [defaultPredicate],
     };
 
     componentDidMount() {
@@ -35,8 +78,37 @@ class Gate extends React.Component <GateProps> {
         this.props.availableRoles.indexOf(this.props.role) !== -1,
         'You have selected an invalid role!',
       );
-      const permissions = this.props.availablePermissions
-        .filter(p => this.props.permissions.indexOf(p.name) !== -1)
-        .map(p => ...p.predicates.map(f => closurize(state, f)))
+      // Permission retrieving
+      const permissions = this.props.permissions
+        .filter(p => this.props.selectedPermissions.indexOf(p.name) !== -1)
+        .reduce((x, y) => x.concat(y), []);
+      this.setState({ permissions });
+    }
+
+    render() {
+      const {
+        action,
+        authConfig,
+        role,
+        onlyLogin,
+        userRole,
+        userObject,
+      } = this.props;
+      if (
+        (!onlyLogin && userRole === role && Predicate.and(...this.state.permissions)()) ||
+            (userObject && onlyLogin)
+      ) {
+        if (action) action(AUTH_SUCCESSFUL);
+        window.console.log('Here', userRole, role);
+        return this.props.children;
+      }
+      if (authConfig.Component404) {
+        if (action) action(AUTH_FAILED);
+        return <authConfig.Component404 />;
+      }
+      if (action) action(AUTH_FAILED);
+      return <Redirect to={authConfig.redirectPath} />;
     }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gate);
