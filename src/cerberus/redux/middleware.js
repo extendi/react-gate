20baf150d4eab20a421fcc59b5ec2106e8b74f9e
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2018-04-17 16:44:05 +0200

    Middleware refactored

--- src/cerberus/redux/middleware.js
+++ src/cerberus/redux/middleware.js
@@ -1,19 +1,13 @@
-import { updateInfo, setupInternals } from './actions';
+import { updateInfo } from './actions';
 
 const defaultRoleSelector = () => undefined; // For the sake of clarity
 
 const createMiddleware = ({
   loginSelector,
   roleSelector = defaultRoleSelector,
-  ...rest
 }) => store => next => (action) => {
   // First get current store
   const actualState = store.getState();
-  // Check if the config is in the state
-  if (!actualState.authProvider.internals) {
-    // Dispatch the auth config in the state
-    store.dispatch(setupInternals(rest));
-  }
   /* eslint-disable no-param-reassign */
   const parsedAuthInfo = [loginSelector, roleSelector].reduce((result, func) => {
     result[func.name] = func(actualState);
@@ -24,9 +18,9 @@ const createMiddleware = ({
     actualState.authProvider.userObject !== parsedAuthInfo.loginSelector ||
       actualState.authProvider.userRole !== parsedAuthInfo.roleSelector
   ) {
-    store.dispatch(updateInfo(parsedAuthInfo));
+    next(updateInfo(parsedAuthInfo));
   }
-  next(action);
+  return next(action);
 };
 
 export default createMiddleware;
