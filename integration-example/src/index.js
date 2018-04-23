79bdb0c8fa5159ae907925e59520a12070d15ca8
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2018-04-23 16:29:09 +0200

    wip

--- integration-example/src/index.js
+++ integration-example/src/index.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import NotFound from './components/NotFound';
 import NoAuth from './components/NoAuth';
+import NotFound from './components/NotFound';
 import userReducer from './reducer';
+import Lol from './authLib/estrajs';
+console.log(Lol)
+/*
+const GateConfig = {
+  roles: ['admin', 'basic'],
+  roleSelector: state => state.user.role,
+  loginSelector: state => state.user.id,
+  redirectPath: '/noauth',
+  Component404: NotFound,
+  permissions: [
+    {
+      name: 'canWrite',
+      predicates: [state => state.canWrite, state => state.canWrite2],
+    },
+    {
+      name: 'canRead',
+      predicates: [state => state.canRead, state => state.canRead2],
+    },
+  ],
+};
 
-const store = createStore(userReducer);
+const { authReducer } = Initializer(GateConfig);
+*/
+const store = createStore(combineReducers({ user: userReducer }));
 
 const App = () => (
   <Provider store={store} >
     <HashRouter>
       <Switch>
         <Route exact path="/home" component={Home} />
+        <Route exact path="/noauth" component={NoAuth} />
         <Route component={Home} />
       </Switch>
     </HashRouter>
