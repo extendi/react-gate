a30296dd2f91e143260b619df6a83c4154afdd2b
Carmine Di Monaco <dimonaco@extendi.it>
2018-04-24 18:21:46 +0200

    fixes

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ set -e
 SHA=`git rev-parse --verify HEAD`
 REPO="git@github.com:extendi/react-gate.git"
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-
+git clone $REPO out
+cd out
+git checkout travis-integration
 # Adding deploy ssh key.
 openssl aes-256-cbc -K $encrypted_9b89288d412e_key -iv $encrypted_9b89288d412e_iv -in keys.enc -out keys -d
 chmod 600 ./keys
