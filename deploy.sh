977ea6c9e0d49daec0810e71c3d27ff715793437
Carmine Di Monaco <dimonaco@extendi.it>
2018-04-24 17:30:23 +0200

    Moved openssl stuff to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 set -e
 SHA=`git rev-parse --verify HEAD`
 # Adding deploy ssh key.
+openssl aes-256-cbc -K $encrypted_4b8c039429e2_key -iv $encrypted_4b8c039429e2_iv -in keys.pub.enc -out keys.pub -d
 chmod 600 ./keys.pub
 eval `ssh-agent -s`
 ssh-add keys.pub
