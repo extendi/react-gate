e36233e1686f1b831169a0958669b1d173a8d450
Carmine Di Monaco <dimonaco@extendi.it>
2018-04-24 17:32:39 +0200

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ eval `ssh-agent -s`
 ssh-add keys.pub
 # Build with webpack and push the library to the repo
 yarn build
-git add .
+git add ./lib
 git commit -m "Building release for commit ${CURRENT_SHA}"
 git push
 
