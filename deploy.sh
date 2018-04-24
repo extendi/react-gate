194dfbb78c87e6e65ce654f0a9ea09b302ffbe0e
Carmine Di Monaco <dimonaco@extendi.it>
2018-04-24 18:26:38 +0200

    order of deploy

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ set -e
 SHA=`git rev-parse --verify HEAD`
 REPO="git@github.com:extendi/react-gate.git"
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-git clone $REPO out
-cd out
-git checkout travis-integration
 # Adding deploy ssh key.
 openssl aes-256-cbc -K $encrypted_9b89288d412e_key -iv $encrypted_9b89288d412e_iv -in keys.enc -out keys -d
 chmod 600 ./keys
 eval `ssh-agent -s`
 ssh-add keys
+git clone $REPO out
+cd out
+git checkout travis-integration
 # Build with webpack and push the library to the repo
 yarn build
 git add ./lib
