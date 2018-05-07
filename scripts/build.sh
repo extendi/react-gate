8953c8e3a242283d48f97d5f38c10394565babc1
Carmine Di Monaco <dimonaco@extendi.it>
2018-05-07 16:33:09 +0200

    Fixed script for building gh pages

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-if [ $TRAVIS_TAG =~ ^pages-release-.*$ ]; then
+if [[ $TRAVIS_TAG =~ ^pages-release-.*$ ]]; then
     mkdir -p ~/.gitbook/versions/$GITBOOK_VERSION/node_modules
     cp -a node_modules/highlight.js/. ~/.gitbook/versions/$GITBOOK_VERSION/node_modules/highlight.js/
     yarn build:doc
