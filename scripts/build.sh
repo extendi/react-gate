463e577a37cbf696467f552eb7967552fdf75015
Carmine Di Monaco <dimonaco@extendi.it>
2018-05-08 14:07:39 +0200

    Added public url env variable to route basepath, in order to deploy react router stuff to github pages.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,9 @@ if [[ $TRAVIS_TAG =~ ^pages-release-.*$ ]]; then
     mkdir -p ~/.gitbook/versions/$GITBOOK_VERSION/node_modules
     cp -a node_modules/highlight.js/. ~/.gitbook/versions/$GITBOOK_VERSION/node_modules/highlight.js/
     yarn build:doc
+    cd integration-example
+    PUBLIC_URL=https://extendi.github.io/react-gate/ yarn build
+    cd ..
     echo "Gitbook has built the doc site for github pages"
 else
     echo "No need to build gitbook stuff."
