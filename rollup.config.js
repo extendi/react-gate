82fd0aa77c3187e6067c466f24ef03ea6becf8fe
Carmine Di Monaco <dimonaco@extendi.it>
2018-05-08 13:45:31 +0200

    Updated rollup configuration [skip ci]

--- rollup.config.js
+++ rollup.config.js
@@ -1,54 +1,64 @@
 import commonjs from 'rollup-plugin-commonjs';
-import filesize from 'rollup-plugin-filesize';
 import replace from 'rollup-plugin-replace';
+import filesize from 'rollup-plugin-filesize';
 import resolve from 'rollup-plugin-node-resolve';
-import sourceMaps from 'rollup-plugin-sourcemaps';
 import uglify from 'rollup-plugin-uglify';
 import flow from 'rollup-plugin-flow';
 import babel from 'rollup-plugin-babel';
+import { minify } from 'uglify-es';
 
-export default {
-  input: 'src/react-gate/index.js',
-  output: [{
-    file: './lib/react-gate.js',
-    format: 'cjs',
-  },
-  {
-    file: './lib/react-gate.es6.js',
-    format: 'es',
-  },
-  {
-    name: 'ReactGate',
-    file: './lib/react-gate.umd.js',
-    format: 'umd',
-  },
-  ],
-  external: ['prop-types', 'redux', 'react', 'react-redux', 'react-router-dom', 'react-dom'],
+const externals = ['prop-types', 'redux', 'react', 'react-redux', 'react-router-dom', 'react-dom'];
+const globals = {
+  'prop-types': 'PropTypes',
+  redux: 'Redux',
+  'react-redux': 'ReactRedux',
+  'react-router-dom': 'ReactRouterDom',
+  'react-dom': 'ReactDom',
+};
+
+const babelConf = {
+  babelrc: false,
+  presets: [['env', { modules: false }], 'stage-0', 'react'],
   plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-    }),
-    flow(),
-    commonjs(),
-    babel({
-      babelrc: false,
-      exclude: 'node_modules/**',
-      presets: [['env', { modules: false }], 'react', 'stage-0', 'flow'],
-      plugins: [
-        ['transform-object-rest-spread'],
-        ['transform-function-bind'],
-        ['transform-class-properties'],
-        ['external-helpers'],
-        ['module-resolver', {
-          root: ['./src'],
-        }],
-      ],
-    }),
-    resolve({
-      extensions: ['.jsx', '.js'],
-    }),
-    sourceMaps(),
-    uglify(),
-    filesize(),
+    ['transform-object-rest-spread'],
+    ['transform-function-bind'],
+    ['transform-class-properties'],
+    ['external-helpers'],
+    ['module-resolver', {
+      root: ['./src'],
+    }],
   ],
 };
+
+export default [
+  {
+    input: 'src/react-gate/index.js',
+    output: {
+      file: './lib/react-gate.es6.js',
+      format: 'es',
+    },
+    external: externals,
+    plugins: [flow(), resolve({ extensions: ['.js', '.jsx'] }), commonjs(), babel(babelConf), filesize()],
+  },
+  {
+    input: 'src/react-gate/index.js',
+    output: {
+      file: './lib/index.js',
+      format: 'umd',
+      globals,
+      name: 'ReactGate',
+    },
+    external: externals,
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      }),
+      flow(),
+      resolve({ extensions: ['.js', '.jsx'] }),
+      commonjs(),
+      babel(babelConf),
+      uglify({}, minify),
+      filesize(),
+    ],
+  },
+];
