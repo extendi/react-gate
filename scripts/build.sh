0b062e297da2c43c740798bb1c9e54b32a505795
Carmine Di Monaco <dimonaco@extendi.it>
2018-05-08 14:28:54 +0200

    Added library build to deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ if [[ $TRAVIS_TAG =~ ^pages-release-.*$ ]]; then
     mkdir -p ~/.gitbook/versions/$GITBOOK_VERSION/node_modules
     cp -a node_modules/highlight.js/. ~/.gitbook/versions/$GITBOOK_VERSION/node_modules/highlight.js/
     yarn build:doc
+    yarn build
     cd integration-example
     yarn install
     PUBLIC_URL=https://extendi.github.io/react-gate/ yarn build
